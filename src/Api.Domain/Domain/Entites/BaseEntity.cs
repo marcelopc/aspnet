b04ae200f726ec0c386cfe9f3ec1ea065087adca
marcelopc <marcelopc04@gmail.com>
2023-04-05 18:08:07 -0300

    Api.Domain-Implementação BaseEntity

---
+++ src/Api.Domain/Domain/Entites/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entites
+{
+    internal abstract class BaseEntity
+    {
+		[Key]
+        public Guid Id { get; set; }
+		private DateTime? _createAt;
+
+		public DateTime? CreateAt
+		{
+			get { return  _createAt; }
+			set {  _createAt = (value == null ? DateTime.UtcNow : value); }
+		}
+
+		private DateTime? _updateAt;
+
+		public DateTime? MyProperty
+		{
+			get { return _updateAt; }
+			set { _updateAt = (value == null ? DateTime.UtcNow : value);  }
+		}
+
+
+	}
+}
