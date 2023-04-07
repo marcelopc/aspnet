863fca393428c8a740ef92e6994c307696af0c17
marcelopc <marcelopc04@gmail.com>
2023-04-07 13:40:26 -0300

    interface para repositorio

---
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entites;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        Task<T> InsertAsync(T item);
+        Task<T> UpdatetAsync(T item);
+        Task<bool> DeleteAsync(Guid id);
+        Task<T> SelectAsync(Guid id);
+        Task<IEnumerable<T>> SelectAsync();
+
+    }
+}
