c98d08d489a5eeb965bdde275f9430ca43bbeab1
marcelopc <marcelopc04@gmail.com>
2023-04-06 22:29:45 -0300

    criado userMap

---
+++ src/Api.Data/Mapping/UserMap.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("users");
+            builder.HasKey(u => u.Id);
+            builder.HasIndex(u => u.Email).IsUnique();
+            builder.Property(u=> u.Name).IsRequired().HasMaxLength(60);
+            builder.Property(u=> u.Email).HasMaxLength(100);
+        }
+    }
+}
