c36c89bbf9fe873fe500c9a828563a136feaba5e
marcelopc <marcelopc04@gmail.com>
2023-04-10 21:39:58 -0300

    Interface userService

---
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entites;
+
+namespace Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll(Guid id);
+
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
+
+    }
+}
